14dd77a4642b6884e6d146b98c218e5a1aa7e32b
Gabriel ML <gabrielmljob@gmail.com>
2025-08-16 20:24:47 +0200

    feat(TDD cycle: Green stage): Implement method to print table.
    It should contain the minimal amount of code needed to make the test
    pass.
    
    This add the logic to the `MultiplicationTable` class.
    Implements the `printTable` method to print the multiplication table of
    the selected number.
    
    `System.out.println` should print the output in the exact format
    required. This is crucial because the test is asserting against this
    specific string format.

--- src/main/java/dev/gml/MultiplicationTable.java
+++ src/main/java/dev/gml/MultiplicationTable.java
@@ -1,5 +1,12 @@
 package dev.gml;
 
 public class MultiplicationTable {
-    
+
+    // METHODS
+    public void printTable(int number) {
+        for (int index = 1; index <= 10; index++) {
+            int result = number * index;
+            System.out.println(number + " x " + index + " = " + result);
+        }
+    }
 }
