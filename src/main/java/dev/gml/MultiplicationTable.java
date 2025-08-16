94f1c5f39d6459dfbc75ac55185ca6233a97ed65
Gabriel ML <gabrielmljob@gmail.com>
2025-08-16 19:16:58 +0200

    feat(TDD cycle: Red stage): Add failing test to print table.
    This new test describe a desired behavior for the `MultiplicationTable`
    class: "It should print the multiplication table by five".
    
    This test "should fail!", because the code to make it pass hasn't been
    written yet (`printTable` method doesn't exist yet).
    
    Issue: "The method printTable(int) is undefined for the type
    MultiplicatonTable"

---
+++ src/main/java/dev/gml/MultiplicationTable.java
@@ -0,0 +1,5 @@
+package dev.gml;
+
+public class MultiplicationTable {
+    
+}
---
+++ src/test/java/dev/gml/MultiplicationTableTest.java
@@ -0,0 +1,53 @@
+package dev.gml;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MultiplicationTableTest {
+    // ATTRIBUTES (aka fields)
+    // Captures output sent to the console.
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    // METHODS
+    @BeforeEach
+    public void setUp() {
+        // It redirects 'System.out' to 'outputStreamCaptor', so I can capture
+        // everything that is printed
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    @DisplayName("1. It should print the multiplication table by five.")
+    void testPrintTableForFive() {
+        // --- Given ---
+        // A multiplication table, a selected number (e.g. 5), and the expected console
+        // output.
+        MultiplicationTable table = new MultiplicationTable();
+        int number = 5;
+        String expectedOutput = "5 x 1 = 5\n" +
+                "5 x 2 = 10\n" +
+                "5 x 3 = 15\n" +
+                "5 x 4 = 20\n" +
+                "5 x 5 = 25\n" +
+                "5 x 6 = 30\n" +
+                "5 x 7 = 35\n" +
+                "5 x 8 = 40\n" +
+                "5 x 9 = 45\n" +
+                "5 x 10 = 50\n";
+
+        // --- When ---
+        // I call the multiplication table by 5 to be printed on the console.
+        table.printTable(number);
+
+        // --- Then ---
+        // I confirm that the actual console output is the same as the expected console
+        // output.
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+    }
+}
